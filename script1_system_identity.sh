5904f0b22755a339960551dfbea2611f9e642397
pranshu-27y <pranshuy27@gmail.com>
2026-03-30 16:10:23 +0530

    Create script1_system_identity.sh

---
+++ script1_system_identity.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script 1: System Identity Report
+
+STUDENT_NAME="Pranshu Yadav"
+SOFTWARE_CHOICE="Git"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(lsb_release -d 2>/dev/null | cut -f2)
+
+echo "=============================="
+echo "Spidey Open Source Audit — $STUDENT_NAME"
+echo "=============================="
+echo "Kernel  : $KERNEL"
+echo "User    : $USER_NAME"
+echo "Uptime  : $UPTIME"
+echo "Date    : $DATE"
+echo "Distro  : $DISTRO"
+echo "License : GNU General Public License"
